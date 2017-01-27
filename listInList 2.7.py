3d1da8b28dfec7d4339bbcfe887940944e5211c4
suryagnfc <suryakislaya@gmail.com>
2017-01-27 16:49:05 +0530

    works in python 3.6
    this file has commands/syntax which is supported by python 3.6 and not
    2.7

---
+++ listInList 2.7.py
@@ -0,0 +1,17 @@
+
+listInList = ['a', 'b', 'c', 'd']
+
+print(listInList[0] + '\n')
+
+for index in range(len(listInList)):
+    print (listInList[index])
+
+print
+
+for index in (listInList):
+    print (index)
+
+print 'rf'
+
+for index in (listInList):
+    print (listInList.index(index))
