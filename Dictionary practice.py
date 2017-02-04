666c61bbef2828a6771e6d908f2f24f4593d956b
suryagnfc <suryakislaya@gmail.com>
2017-02-04 23:19:29 +0530

    dictonaryPratice
    Dictonary practice programs

---
+++ Dictionary practice.py
@@ -0,0 +1,34 @@
+
+dictionaryItem = {'abc':'news channel', 2 : '2nd item', '3rd item' : 3,
+                  '4th item': 4,
+                  '5th item' : 5.0}
+
+#print dictionaryItem[0]
+
+for key, value in dictionaryItem.items():
+    print (str(key) + ' is of type' + str(type(key)) + ' value =' + str(value) +
+           ' is of type:' + str(type(value)))
+
+dictionaryInception = {'abc' : 'not news channel',
+                       'player2' : { 'level':30,
+                                     'shieldDateTime': '25/11/2016_12:23:12',
+                                     'friendsNumber':30,
+                                     'pointsPurchased':230,
+                                     'lastLoginDate', '12/01/2017':
+                                     'lastLoginTime', '12:34:34':
+                                     },
+                       'player3' : { 'level':05,
+                                     'shieldDateTime': '25/11/2016_12:23:12',
+                                     'friendsNumber':12
+                                     'pointsPurchased':1045,
+                                     'lastLoginDate', '12/12/2016':
+                                     'lastLoginTime', '11:23:59':
+                                     },
+                       'player4' : { 'level':30,
+                                     'shieldDateTime': '25/11/2016_12:23:12',
+                                     'friendsNumber':30,
+                                     'pointsPurchased':230,
+                                     'lastLoginDate', '12/01/2017':
+                                     'lastLoginTime', '12:34:34':
+                                     },
+                       
