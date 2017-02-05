a0ca41f7e48438ac46d7e618a7c89f6177ccbd05
suryagnfc <suryakislaya@gmail.com>
2017-02-05 18:55:58 +0530

    modifyIO for getting game number with reg ex

--- fileIO.py
+++ fileIO.py
@@ -1,12 +1,23 @@
 import os
+import re
 
 if (os.path.exists(os.path.join(os.getcwd(),'TestFiles')) != True):
     os.makedirs(os.path.join(os.getcwd(),'TestFiles'))
 
 path = os.path.join(os.getcwd(),'TestFiles')
 print(path)
-fileName = path + '\\'+ 'TicTacToe.txt'
+fileName = path + '\\'+ 'test_game number.txt'
 print(fileName)
-fileObj = open(fileName, 'w')
-fileObj.write('X won')
+fileObj = open(fileName, 'r')
+existingFileContent = fileObj.read()
+
+gameRegEx= re.compile(r'game \d')
+mo = gameRegEx.findall(existingFileContent)
+lastGame = mo[len(mo)-1]
+
+lastGameNumber = lastGame.split(' ')
+print(int(lastGameNumber[1]))
+print(mo[len(mo)-1])
+
+#fileObj.write('X won')
 fileObj.close()
